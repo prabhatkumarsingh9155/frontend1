918549e8d6b80986cdcf5f2f05f2a99f3a741564
prabhatkumarsingh9155 <prabhatkumarsingh9155@gmail.com>
2023-08-20 15:09:14 +0530

    update css code

---
+++ project/TO DO API/js/create.js
@@ -0,0 +1,12 @@
+let formId=document.getElementById('create-todo');
+let fTodo=document.getElementById('todo');
+let fCompleted=document.getElementById('completed');
+//generating random uder id 
+function genRandId(){
+return Math.floor(Math.random()*1000);
+ 
+
+
+
+}
+formId.addEventListener
